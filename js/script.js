d5eb168a788073f9b7fb721703ac2b4db2bc4f41
Kyle Ines <kyleines@gmail.com>
2021-03-17 16:56:07 -0500

    Add showPage() function

--- js/script.js
+++ js/script.js
@@ -3,23 +3,50 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+function showPage(list, page) {
+   let startingIndex = page * 9 - 9;
+   let endingIndex = page * 9 - 1;
+   const studentList = document.querySelector(".student-list");
+   studentList.innerHTML = "";
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startingIndex && i <= endingIndex) {
+         const student = list[i];
+         const li = document.createElement("li");
+         li.className = "student-item cf";
 
+         const studentDiv = document.createElement("div");
+         studentDiv.className = "student-details";
+         const img = document.createElement("img");
+         img.className = "avatar";
+         img.src = student.picture.large;
+         img.alt = "Profile Picture";
+         studentDiv.appendChild(img);
+         const studentName = document.createElement("h3");
+         studentName.textContent = `${student.name.first} ${student.name.last}`;
+         studentDiv.appendChild(studentName);
+         const emailSpan = document.createElement("span");
+         emailSpan.className = "email";
+         emailSpan.textContent = student.email;
+         studentDiv.appendChild(emailSpan);
+         li.appendChild(studentDiv);
+         
+         const detailsDiv = document.createElement("div");
+         detailsDiv.className = "joined-details";
+         const joinedSpan = document.createElement("span");
+         joinedSpan.className = "date";
+         joinedSpan.textContent = `Joined ${student.registered.date}`;
+         detailsDiv.appendChild(joinedSpan);
+         li.appendChild(detailsDiv);
 
+         studentList.appendChild(li);
+      }
+   }
+}
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
@@ -28,3 +55,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showPage(data, 1);
