6e8fcc8716abe0502f0be2afd056210e809cda3e
Kyle Ines <kyleines@gmail.com>
2021-03-18 09:13:19 -0500

    Refactor js functions

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ FSJS Project 2 - Data Pagination and Filtering
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-
 function showPage(list, page) {
    let startingIndex = page * 9 - 9;
    let endingIndex = page * 9 - 1;
@@ -16,68 +15,72 @@ function showPage(list, page) {
    for (let i = 0; i < list.length; i++) {
       if (i >= startingIndex && i <= endingIndex) {
          const student = list[i];
-         const li = document.createElement("li");
-         li.className = "student-item cf";
-
-         const studentDiv = document.createElement("div");
-         studentDiv.className = "student-details";
-         const img = document.createElement("img");
-         img.className = "avatar";
-         img.src = student.picture.large;
-         img.alt = "Profile Picture";
-         studentDiv.appendChild(img);
-         const studentName = document.createElement("h3");
-         studentName.textContent = `${student.name.first} ${student.name.last}`;
-         studentDiv.appendChild(studentName);
-         const emailSpan = document.createElement("span");
-         emailSpan.className = "email";
-         emailSpan.textContent = student.email;
-         studentDiv.appendChild(emailSpan);
+         const li = createElement("li", "className", "student-item cf");
+         const studentDiv = createElement("div", "className", "student-details");
+         appendElement(studentDiv, "img", "className", "avatar", "src", student.picture.large, "alt", "Profile Picture");
+         appendElement(studentDiv, "h3", "textContent", `${student.name.first} ${student.name.last}`);
+         appendElement(studentDiv, "span", "className", "email", "textContent", student.email);
          li.appendChild(studentDiv);
-         
-         const detailsDiv = document.createElement("div");
-         detailsDiv.className = "joined-details";
-         const joinedSpan = document.createElement("span");
-         joinedSpan.className = "date";
-         joinedSpan.textContent = `Joined ${student.registered.date}`;
-         detailsDiv.appendChild(joinedSpan);
+         const detailsDiv = createElement("div", "className", "joined-details");
+         appendElement(detailsDiv, "span", "className", "date", "textContent", `Joined ${student.registered.date}`);
          li.appendChild(detailsDiv);
-
          studentList.appendChild(li);
       }
    }
 }
+
+
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
 function addPagination(list) {
    let pages = Math.round(list.length / 9);
    const linkList = document.querySelector(".link-list");
    linkList.innerHTML = "";
    for (let i = 1; i <= pages; i++) {
-      const li = document.createElement("li");
-      const button = document.createElement("button");
-      button.type = "button";
-      button.textContent = i;
-      li.appendChild(button);
+      const li = createElement("li");
+      appendElement(li, "button", "type", "button", "textContent", i);
       linkList.appendChild(li);
    }
-
    const firstPageButton = document.querySelector("button");
    firstPageButton.className = "active";
-
    linkList.addEventListener("click", (e) => {
       const buttons = document.getElementsByTagName("BUTTON");
       if (e.target.tagName === "BUTTON") {
-         for (let button of buttons) {
+         for (let button in buttons) {
             button.className = "";
          }
          e.target.className = "active";
          showPage(list, e.target.textContent)
       }
    });
+}
+
+/*
+Create the `createElement` function
+This function will create elements needed for the showPage() and addPagination() functions
+*/
+function createElement(elementName, property1, value1, property2, value2, property3, value3) {
+   const element = document.createElement(elementName);
+   element[property1] = value1;
+   if (property2) {
+      element[property2] = value2;
+   }
+   if (property3) {
+      element[property3] = value3;
+   }
+   return element;
+}
+
+/*
+Create the `appendElement` function
+This function will append elements once created with the createElement() function
+*/
+function appendElement(parent, elementName, property1, value1, property2, value2, property3, value3) {
+   const element = createElement(elementName, property1, value1, property2, value2, property3, value3);
+   parent.appendChild(element);
+   return element;
 }
 
 // Call functions
